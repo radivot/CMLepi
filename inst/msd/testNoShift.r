b39f9acdee5dfb145c457e2097e5774ade83b43d
Tomas Radivoyevitch <radivot@gmail.com>
2026-09-15 05:38:50 -0400

    increase res to see round off error

--- inst/msd/testNoShift.r
+++ inst/msd/testNoShift.r
@@ -1,26 +1,24 @@
 # testNoShift.R   Shows that if there are no agedx shifts, SEERaBomb's matrix approach matches the biostat3 approach  
 graphics.off();rm(list=ls())#clear plots and environment 
 library(tidyverse)  
+options(pillar.sigfig = 6) # Show 6 significant digits 
 load("~/data/CMLepi/cml.RData") #made in mkSEER.R  53.2k
 d=d|>mutate(agedx=ifelse(agedx==90,92.3,agedx)) # set over 90 to 92.3
 d=d|>mutate(surv=ifelse(surv>80,0.01,surv)) #set NA surv to 0.01 (3.65 days)
 d=d|>mutate(surv=ifelse(surv==0,0.01,surv)) #set  0 surv to 0.01 (else survSplit throws 'zero' parameter must be less than any observed times)
 load("~/data/mrt/mrtUSA.RData")#mrt is list of 3 matrices
 d=d|>select(yrdx,agedx,sex,surv,status) # 43,932 CML cases
-(D=SEERaBomb::msd(d,mrt,brkst=c(0)))
-# # A tibble: 2 × 12
-#   int         t     O     E      PY    EAR     LL     UL    RR   rrL   rrU sex   
-#   <fct>   <dbl> <dbl> <dbl>   <dbl>  <dbl>  <dbl>  <dbl> <dbl> <dbl> <dbl> <chr> 
-# 1 (0,100]  3.23 14913 4055. 194493. 0.0558 0.0546 0.0571  3.68  3.62  3.74 Male  
-# 2 (0,100]  3.32 10918 2702. 153663. 0.0535 0.0521 0.0548  4.04  3.96  4.12 Female
+SEERaBomb::msd(d,mrt,brkst=0)|>select(O:sex)|>relocate(sex,.before=O)|>arrange(sex)
+#  sex        O       E      PY       EAR        LL        UL      RR     rrL     rrU
+# 1 Female 10918 2702.49 153663. 0.0534646 0.0521318 0.0547974 4.03998 3.96455 4.11648
+# 2 Male   14913 4054.66 194493. 0.0558291 0.0545984 0.0570597 3.67799 3.61920 3.73751
 load("~/data/CMLepi/daytsNoShift.RData") #adx=agedx, not adx=agedx+0.5 
 dayts=dayts|>mutate(E=PY*m)
 d0=dayts|>group_by(sex)|>summarize(O=sum(O),E=sum(E),PY=sum(PY))
 d0|>mutate(EAR=(O-E)/PY,LL=EAR-1.96*sqrt(O)/PY,UL=EAR+1.96*sqrt(O)/PY,
              RR=O/E,rrL=qchisq(.025,2*O)/(2*E),rrU=qchisq(.975,2*O+2)/(2*E)) 
-#   sex        O     E      PY    EAR     LL     UL    RR   rrL   rrU
-#   <chr>  <dbl> <dbl>   <dbl>  <dbl>  <dbl>  <dbl> <dbl> <dbl> <dbl>
-# 1 Female 10918 2703. 153663. 0.0535 0.0521 0.0548  4.04  3.96  4.12
-# 2 Male   14913 4055. 194493. 0.0558 0.0546 0.0571  3.68  3.62  3.74
-# matches above perfectly, so it seems msd() has been working fine 
+#  sex        O       E      PY       EAR        LL        UL      RR     rrL     rrU
+# 1 Female 10918 2702.55 153663. 0.0534642 0.0521314 0.0547970 4.03989 3.96446 4.11639
+# 2 Male   14913 4055.09 194493. 0.0558269 0.0545962 0.0570575 3.67760 3.61881 3.73711
+# small diffs in E values could be round off errors, i.e. it seems msd() has been working pretty fine 
 
--- inst/msd/testPreShift.r
+++ inst/msd/testPreShift.r
@@ -1,6 +1,7 @@
 # testPreShift.R  Shows that adding 0.5 to agedx upfront with msd equals adding it to both agedx and yrdx upfront for survSplit
 graphics.off();rm(list=ls())#clear plots and environment 
 library(tidyverse)  
+options(pillar.sigfig = 6) # Show 6 significant digits 
 load("~/data/CMLepi/cml.RData") #made in mkSEER.R  53.2k
 d=d|>mutate(agedx=agedx+0.5)
 d=d|>mutate(agedx=ifelse(agedx>90,92.3,agedx)) # set over 90 to 92.3
@@ -8,21 +9,16 @@ d=d|>mutate(surv=ifelse(surv>80,0.01,surv)) #set NA surv to 0.01 (3.65 days)
 d=d|>mutate(surv=ifelse(surv==0,0.01,surv)) #set  0 surv to 0.01 (else survSplit throws 'zero' parameter must be less than any observed times)
 d=d|>select(yrdx,agedx,sex,surv,status) # 43,932 CML cases
 load("~/data/mrt/mrtUSA.RData")#mrt is list of 3 matrices
-(D=SEERaBomb::msd(d,mrt,brkst=c(0)))
-#  A tibble: 2 × 12
-#   int         t     O     E      PY    EAR     LL     UL    RR   rrL   rrU sex   
-#   <fct>   <dbl> <dbl> <dbl>   <dbl>  <dbl>  <dbl>  <dbl> <dbl> <dbl> <dbl> <chr> 
-# 1 (0,100]  3.23 14913 4212. 194493. 0.0550 0.0538 0.0563  3.54  3.48  3.60 Male  
-# 2 (0,100]  3.32 10918 2822. 153663. 0.0527 0.0514 0.0540  3.87  3.80  3.94 Female
+SEERaBomb::msd(d,mrt,brkst=0)|>select(O:sex)|>relocate(sex,.before=O)|>arrange(sex)
+#   sex        O       E      PY       EAR        LL        UL      RR     rrL     rrU
+# 1 Female 10918 2822.12 153663. 0.0526861 0.0513533 0.0540188 3.86872 3.79649 3.94198
+# 2 Male   14913 4211.69 194493. 0.0550217 0.0537911 0.0562524 3.54086 3.48426 3.59815
 load("~/data/CMLepi/daysPreShift.RData")
 days=days|>mutate(E=PY*m)
 d0=days|>group_by(sex)|>summarize(O=sum(O),E=sum(E),PY=sum(PY))
 d0|>mutate(EAR=(O-E)/PY,LL=EAR-1.96*sqrt(O)/PY,UL=EAR+1.96*sqrt(O)/PY,
              RR=O/E,rrL=qchisq(.025,2*O)/(2*E),rrU=qchisq(.975,2*O+2)/(2*E)) 
-# # A tibble: 2 × 10
-#   sex        O     E      PY    EAR     LL     UL    RR   rrL   rrU
-#   <chr>  <dbl> <dbl>   <dbl>  <dbl>  <dbl>  <dbl> <dbl> <dbl> <dbl>
-# 1 Female 10918 2831. 153663. 0.0526 0.0513 0.0540  3.86  3.78  3.93
-# 2 Male   14913 4231. 194493. 0.0549 0.0537 0.0562  3.52  3.47  3.58
-# So, while O and PY are still the same,  E is now bigger: 4212=>4231 and 2822=>2831. 
-range(dayts$year)
+#   sex        O       E      PY       EAR        LL        UL      RR     rrL     rrU
+# 1 Female 10918 2821.94 153663. 0.0526872 0.0513544 0.0540200 3.86897 3.79673 3.94223
+# 2 Male   14913 4211.44 194493. 0.0550230 0.0537923 0.0562536 3.54107 3.48446 3.59836
+### there's a tiny bit of round off error diffs in E
