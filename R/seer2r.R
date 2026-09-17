6887891cd920f3448a3d447bb1d1bf189f9b3615
Tomas Radivoyevitch <radivot@gmail.com>
2026-09-17 13:31:56 -0400

    add COD12 to look at BEN, MMC and OCD as maybe picking up some zoomed out excess LC

--- R/seer2r.R
+++ R/seer2r.R
@@ -70,5 +70,39 @@ seer2r=function(db,path="~/data/CMLepi") {
     D$COD7=CODt
     D|>relocate(COD7, .after = COD2)
   }
-  mapCOD7(d)
+  d=mapCOD7(d)
+  mapCOD12=function(D){
+    COD=D$COD #start with vec of integers. Map to a vec of Strings
+    CODt=rep("UNK",dim(D)[1]) #set default to "unknown" type of death
+    CODt[COD==0]="alive"
+    CODt[(COD>=1)&(COD<=73)|(COD==90)]="CA"
+    CODt[(COD>=74)&(COD<=85)|(COD==89)]="LC"
+    CODt[COD==86]="MMC" #Miscellaneous Malignant Cancer
+    CODt[COD==130]="BEN" #in situ (benign)
+    CODt[(COD>=133)&(COD<=145)]="IN" # infection
+    CODt[COD==148]="DK"  # diabetes
+    CODt[COD==151]="MSC"  # alzheimers -> miscellaneous specific cause
+    CODt[COD==154]="CV"  # heart disease
+    CODt[COD==157]="CV" # hypertension without HD
+    CODt[COD==160]="CV"  #cerebroVasc"
+    CODt[COD==163]= "CV" #"athero"
+    CODt[COD==166]= "CV"     #"aoritic aneurysm"
+    CODt[COD==169]= "CV"  #"other disease of Vasc"
+    CODt[COD==172]= "IN" #"pneumonia"
+    CODt[COD==175]="COPD" # COPD, chronic obstructive pulminary disease
+    CODt[COD==178]="MSC" # ulcer
+    CODt[COD==181]="MSC" # liver disease
+    CODt[COD==184]="DK" # kidney disease
+    CODt[COD==196]="ILL" # ill-defined and unknown"
+    CODt[COD==199]="ASH" #"accidents"
+    CODt[COD==202]="ASH"  #"suicide"
+    CODt[COD==205]="ASH" # homocide"
+    CODt[COD%in%c(187,190,193)]="MSC" # perinatal conditions
+    CODt[COD==208]="OCD" # Other Causes of Death = 208
+    CODt[COD==252]="ILL" # no DC or DC but no COD => send to smaller set ILL to keep OCD clean
+    # CODt[COD==252]="UNK" # same if no comment => all accounted for
+    D$COD12=CODt
+    D|>relocate(COD12, .after = COD7)
+  }
+  mapCOD12(d)
 }
